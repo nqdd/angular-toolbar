a4ef52c24a0f298ba21231e4fd4d6b67df9952b4
Dat Nguyen Quoc <datnq1210gmail.com>
2023-12-04 15:37:00 +0700

    chore: add tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-toolbar [elementGap]="8">
-      <ng-container *ngFor="let _ of [].constructor(100); let idx = index">
-        <ng-template appToolbarItemTemplate>
-          <button>Button {{ idx }}</button>
-        </ng-template>
-      </ng-container>
-      <ng-template appToolbarCollapseButtonTemplate>More</ng-template>
-    </app-toolbar>
+    <div class="flex items-center justify-items-center">
+      <app-toolbar [elementGap]="8">
+        <ng-container *ngFor="let _ of [].constructor(100); let idx = index">
+          <ng-template appToolbarItemTemplate>
+            <button>Button {{ idx }}</button>
+          </ng-template>
+        </ng-container>
+        <ng-template appToolbarCollapseButtonTemplate>More</ng-template>
+      </app-toolbar>
+    </div>
   `,
   styles: [],
 })
